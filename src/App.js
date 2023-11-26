263aca73fe8d321893388223e561fea65788c3da
001azim <star.azim776@gmail.com>
2023-11-26 09:25:09 -0800

    jj

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+   <>
+  <h1>rmm</h1>
+  <h2>rfttttttttttttttt</h2>
+   </>
+    
+     
+  );
+}
+
+export default App;
